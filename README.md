0045d38470c602c9880ab4cc5c832c3cdc04671a
Mikhail Trishin <maikltrishin@yandex.ru>
2024-08-21 18:49:38 +0300

    one_commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # qa_python
+Список реализованых тестов:
+test_add_new_book_true - добавляем новую книгу
+test_add_new_book_no_gerne - проверяем что книга добавилась без жанра
+test_set_book_genre_true - добавляем книге жанр
+test_get_books_with_specific_genre_true - вывод книг определенного жанра
--- tests.py
+++ tests.py
@@ -2,35 +2,21 @@ import pytest
 from main import BooksCollector
 
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert len(collector.get_books_genre()) == 2
 
 
-@pytest.mark.parametrize('book',['Гарри Поттер','Стихи Пушкина','Властелин колец'])
-    def test_add_new_book_true(self,book):
+    def test_add_new_book_true(self):
         collector = BooksCollector()
-        collector.add_new_book(book)
-        assert book != 0
+        collector.add_new_book('Гарри Поттер')
+        assert len(collector.get_books_genre()) == 1
+
 
 
     def test_add_new_book_no_gerne(self):
@@ -43,6 +29,7 @@ class TestBooksCollector:
         book = collector.add_new_book('Гарри Поттер')
         collector.books_genre[book] = collector.genre_age_rating[0]
         assert collector.books_genre[book]
+
 
     @pytest.mark.parametrize('book' ,['Оно','Король лев'])
     def test_get_books_with_specific_genre_true(self,book):
@@ -62,16 +49,17 @@ class TestBooksCollector:
 
     def test_get_books_genre_true(self):
         collector = BooksCollector()
-        book = collector.get_books_genre()
-        assert book != None
+        collector.add_new_book('Гарри Поттер')
+        books = collector.get_books_genre()
+        assert len(books) == 1
 
-    def test_get_boofs_for_children(self):
+    def test_get_books_for_children(self):
         collector = BooksCollector()
         book = 'Король Лев'
         genre = 'Мультфильмы'
         collector.add_new_book(book)
-        collector.set_book_genre(book,genre)
-        assert collector.get_books_for_children()
+        book = collector.set_book_genre(book,genre)
+        assert collector.get_books_for_children()==['Король Лев']
 
 
     def test_add_book_in_favorites_true(self):
@@ -79,7 +67,7 @@ class TestBooksCollector:
         collector.add_new_book('Гарри Поттер')
         collector.add_book_in_favorites('Гарри Поттер')
         favorites = collector.get_list_of_favorites_books()
-        assert len(favorites) != 0
+        assert len(favorites) == 1
 
 
     def test_delete_book_from_favorites_true(self):
@@ -94,8 +82,11 @@ class TestBooksCollector:
 
     def test_get_list_of_favorites_books_true(self):
         collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер','Фантастика')
+        collector.add_book_in_favorites('Гарри Поттер')
         book = collector.get_list_of_favorites_books()
-        assert book != None
+        assert book == ['Гарри Поттер']
 
 
 
